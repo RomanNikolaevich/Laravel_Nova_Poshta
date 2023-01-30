a60c3dc50d3d987f63f5e588e04b84e710fdb4e3
RomanNikolaevich <roman.nikolaevich@gmail.com>
2023-01-30 22:05:54 +0200

    warehouse controller +service changes

---
+++ app/Http/Services/WarehouseService.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Http\Controllers\CityController;
+use App\Models\Warehouse;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Http;
+use JsonException;
+
+class WarehouseService
+{
+    /**
+     * @return Application|Factory|View
+     */
+    public function getListFromDB():Application|Factory|View
+    {
+        $warehouses = Warehouse::get();
+        return view('index', ['warehouses'=>$warehouses]);
+    }
+
+    /**
+     * Get data from Api Niva Poshta
+     * @throws JsonException
+     */
+    public function getDataFromApiNP():array
+    {
+        $url = config('novaposhta.url');
+        $data = config('novaposhta.data_warehouse');
+
+        $response = Http::post($url, $data);
+
+        //Get all warehouses
+        $warehouses = json_decode($response, true, 512, JSON_THROW_ON_ERROR)["data"];//array
+        $cities = app(CityController::class)->getListFromDB();
+
+        $cities = $cities['cities']->toArray();
+
+        $filtered_warehouses = [];
+        foreach ($warehouses as $warehouse) {
+            foreach ($cities as $city) {
+                if ($city['ref'] === $warehouse['CityRef']) {
+                    $filtered_warehouses[] = $warehouse;
+                }
+            }
+        }
+
+        return $filtered_warehouses;
+    }
+
+    /**
+     * Store data in database
+     *
+     * @return void
+     * @throws JsonException
+     */
+    public function storeDataInDatabase():void
+    {
+        foreach ($this->getDataFromApiNP() as $warehouse) {
+            Warehouse::create([
+                'city_ref'       => $warehouse['CityRef'],
+                'description'    => $warehouse['Description'],
+                'description_ru' => $warehouse['DescriptionRu'],
+            ]);
+        }
+    }
+}
